1944c41109984eca74f86055b1ec2b0cca28668c
ayushi1998 <ayushi16025@iiitd.ac.in>
2017-11-02 13:38:10 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -9,19 +9,33 @@ import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	static Stage stage;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			stage=primaryStage;
 			//BorderPane root = new BorderPane();
-			Parent root= FXMLLoader.load(getClass().getResource("View_Schedule.fxml"));
+			Parent root= FXMLLoader.load(getClass().getResource("Sign_Up.fxml"));
 			Scene scene = new Scene(root,600,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			//signup_controller sc=new signup_controller(primaryStage);
+			
+			
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+		
 	}
+	
+	
+	
 	
 	public static void main(String[] args) {
 		launch(args);

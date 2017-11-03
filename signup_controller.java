e7e16be3478e4ebac677844a851c40c28590f979
ayushi1998 <ayushi16025@iiitd.ac.in>
2017-11-03 12:15:32 +0530

    Add files via upload

--- signup_controller.java
+++ signup_controller.java
@@ -1,13 +1,19 @@
 package application;
 
-import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
+import classes.User;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -15,6 +21,22 @@ public class signup_controller
 {
 	@FXML
 	Button login;
+	@FXML
+	Button submit;
+	
+	@FXML
+	TextField username;
+	@FXML
+	TextField email;
+	
+	@FXML
+	PasswordField password;
+	
+	@FXML 
+	ComboBox combo;
+	
+	ArrayList<User> users;
+	
 
 	public void login_click(ActionEvent event) throws Exception
 	{
@@ -22,14 +44,52 @@ public class signup_controller
 		Pane root= FXMLLoader.load(getClass().getResource("Login.fxml"));
 		Scene scene = new Scene(root,600,400);
 		//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-	
-		
 		//pane.getChildren().setAll(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		System.out.println("hello");
 	}
 	
+	public void submit_click(ActionEvent event) throws Exception
+	{
+		/* 1. Taking inputs form the user.
+		 * 2.User object
+		 * 3. Serializing
+		 */
+		//ComboBox 
+		
+		User u=new User(username.getText(), email.getText(), password.getText(), combo.getValue().toString());
+		
+		ObjectOutputStream UsersList	=	null;	
+		try	
+		{	
+			UsersList=new ObjectOutputStream(new FileOutputStream("Users.txt"));	
+			UsersList.writeObject(u);	
+					
+		}	
+		finally	
+		{	
+			UsersList.close();	
+		}	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	@FXML
+	public  void initialize()
+	{
+		combo.getItems().addAll("Faculty", "Student", "Admin");
+		combo.setEditable(true);
+	    combo.setPromptText("Enter");
+	  
+	}
+
 	
 	
 
